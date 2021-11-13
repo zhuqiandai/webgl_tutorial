04a7395c8a78b6719b7c41ee836d3cb6307509d3
zhuqd <1132154046@qq.com>
2021-11-13 12:25:59 +0800

    add

--- src/webgl_4_review/main.js
+++ src/webgl_4_review/main.js
@@ -8,17 +8,26 @@ const gl = webglContainer.getContext('webgl')
 const vsSource = `
 
   attribute vec4 aVertexPosition;
+  attribute vec4 aVertexColor;
 
   uniform mat4 uModelMatrix;
+  uniform mat4 uViewMatrix;
+
+  varying lowp vec4 vColor;
 
   void main(void) {
-    gl_Position = uModelMatrix * aVertexPosition;
+    gl_Position = uViewMatrix * uModelMatrix * aVertexPosition;
+
+    vColor = aVertexColor;
   }
 `
 
 const fsSource = `
+  varying lowp vec4 vColor;
+
   void main(void) {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    gl_FragColor = vColor;
+    // gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
   } 
 `
 
@@ -28,9 +37,11 @@ const programInfo = {
   program,
   attribLocations: {
     aVertexPosition: gl.getAttribLocation(program, 'aVertexPosition'),
+    aVertexColor: gl.getAttribLocation(program, 'aVertexColor'),
   },
   uniformLocations: {
     uModelMatrix: gl.getUniformLocation(program, 'uModelMatrix'),
+    uViewMatrix: gl.getUniformLocation(program, 'uViewMatrix'),
   },
 }
 
@@ -43,35 +54,90 @@ function initBuffers(gl) {
 
   // prettier-ignore
   const positions = [
-    -0.1, -0.1, 0.0,
-    0.1, -0.1, 0.0,
-    0, 0.1, 0.0,
+        // Front face
+    -1.0, -1.0,  1.0, 
+    1.0, -1.0,  1.0, 
+    1.0,  1.0,  1.0, 
+  -1.0,  1.0,  1.0,   
 
-    0.1, 0.1, 0.0,
-    0.3, 0.1, 0.0,
-    0, 0.3, 0.0,
+  // Back face
+  -1.0, -1.0, -1.0,
+  -1.0,  1.0, -1.0,  
+    1.0,  1.0, -1.0, 
+    1.0, -1.0, -1.0,  
+
+  // Top face
+  -1.0,  1.0, -1.0, 
+  -1.0,  1.0,  1.0, 
+    1.0,  1.0,  1.0,
+    1.0,  1.0, -1.0,
+
+  // Bottom face
+  -1.0, -1.0, -1.0,   
+    1.0, -1.0, -1.0, 
+    1.0, -1.0,  1.0,
+  -1.0, -1.0,  1.0, 
+
+  // Right face
+    1.0, -1.0, -1.0,   
+    1.0,  1.0, -1.0, 
+    1.0,  1.0,  1.0,
+    1.0, -1.0,  1.0, 
+
+  // Left face
+  -1.0, -1.0, -1.0, 
+  -1.0, -1.0,  1.0,
+  -1.0,  1.0,  1.0,
+  -1.0,  1.0, -1.0,
   ]
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
+
+  const colorBuffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+  const red = [1.0, 0.0, 0.0, 1.0]
+  const green = [0.0, 1.0, 0.0, 1.0]
+  const yellow = [0.0, 0.0, 1.0, 1.0]
+  const black = [10.0, 0.0, 0.0, 1.0]
+
+  const colors = []
+
+  let i = 0
+  while (i < 6) {
+    colors.push(...red, ...green, ...yellow, ...black)
+
+    i += 1
+  }
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW)
 
   const indexBuffer = gl.createBuffer()
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
 
-  const index = [0, 1, 2, 3, 4, 5]
-
+  // prettier-ignore
+  const index = [
+    0,  1,  2,      0,  2,  3,    // front
+    4,  5,  6,      4,  6,  7,    // back
+    8,  9,  10,     8,  10, 11,   // top
+    12, 13, 14,     12, 14, 15,   // bottom
+    16, 17, 18,     16, 18, 19,   // right
+    20, 21, 22,     20, 22, 23,   // left
+  ];
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(index), gl.STATIC_DRAW)
 
   return {
     positionBuffer,
+    colorBuffer,
     indexBuffer,
   }
 }
 
-function draw(gl) {
+let radians = 0
+function draw(gl, deltaTime) {
   gl.clearColor(0.0, 0.0, 0.0, 1)
 
   gl.clear(gl.COLOR_BUFFER_BIT)
 
-  const { positionBuffer, indexBuffer } = initBuffers(gl)
+  const { positionBuffer, colorBuffer, indexBuffer } = initBuffers(gl)
 
   {
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
@@ -88,20 +154,58 @@ function draw(gl) {
   }
 
   {
-    const modelMatrix = mat4.create()
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
 
-    const location = programInfo.uniformLocations.uModelMatrix
-    gl.uniformMatrix4fv(location, false, modelMatrix)
+    const index = programInfo.attribLocations.aVertexColor
+    const size = 4
+    const type = gl.FLOAT
+    const normalized = false
+    const stride = 0
+    const offset = 0
+
+    gl.vertexAttribPointer(index, size, type, normalized, stride, offset)
+
+    gl.enableVertexAttribArray(index)
   }
 
   {
-    // const first = 0
-    // const count = 6
-    // gl.drawArrays(gl.TRIANGLE_FAN, first, count)
+    const modelMatrix = mat4.create()
+    mat4.scale(modelMatrix, modelMatrix, [0.1, 0.1, 0.1, 1])
+    mat4.rotateX(modelMatrix, modelMatrix, radians)
+    mat4.rotateY(modelMatrix, modelMatrix, radians)
+
+    const modelLocation = programInfo.uniformLocations.uModelMatrix
+    gl.uniformMatrix4fv(modelLocation, false, modelMatrix)
+
+    const viewMatrix = mat4.create()
+    mat4.lookAt(viewMatrix, [0, 0, 0], [0, 0, 1], [0, 1, 0])
+
+    const viewLocation = programInfo.uniformLocations.uViewMatrix
+    gl.uniformMatrix4fv(viewLocation, false, viewMatrix)
+  }
+
+  {
+    const count = 26
+    const type = gl.UNSIGNED_SHORT
+    const offset = 0
 
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
-    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0)
+    gl.drawElements(gl.TRIANGLES, count, type, offset)
   }
+
+  radians += deltaTime
 }
 
-draw(gl)
+let then = 0
+
+function render(now) {
+  now *= 0.001 // convert to seconds
+  const deltaTime = now - then
+  then = now
+
+  draw(gl, deltaTime)
+
+  requestAnimationFrame(render)
+}
+
+requestAnimationFrame(render)
